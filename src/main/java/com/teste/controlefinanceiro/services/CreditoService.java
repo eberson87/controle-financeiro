8cf9455b9075ddb14cb477734906a39cb1eeaf9a
Eberson de Paula <eberson_cirilo@hotmail.com>
2023-08-22 20:01:13 -0300

    :tada: subindo projeto controle financeiro

---
+++ src/main/java/com/teste/controlefinanceiro/services/CreditoService.java
@@ -0,0 +1,66 @@
+package com.teste.controlefinanceiro.services;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.teste.controlefinanceiro.model.Credito;
+import com.teste.controlefinanceiro.respository.CreditoRepository;
+
+@Service
+public class CreditoService {
+
+    @Autowired
+    private CreditoRepository creditoRepository;
+
+    /**
+     * Metodo para retornar receitas
+     * 
+     * @return Lista de receitas
+     */
+    public List<Credito> obterTodos() {
+        return creditoRepository.obterTodos();
+    }
+
+    /**
+     * Metodo que retorna o produto encontrado pelo seu Id.
+     * 
+     * @param id do crédito que será localizado.
+     * @return Retorna uma receita caso tenha sido localizado.
+     */
+    public Optional<Credito> obterPorid(Integer id) {
+        return creditoRepository.obterPorid(id);
+    }
+
+    /**
+     * Metodo para adicionar credito
+     * 
+     * @param credito a que será adicionado
+     * @return retorna o produto que foi adicionado.
+     */
+    public Credito adicionar(Credito credito) {
+        return creditoRepository.adicionar(credito);
+    }
+
+    /**
+     * Metodo para deletar o credito por id.
+     * 
+     * @param id credito a ser deletado
+     */
+    public void deletar(Integer id) {
+        creditoRepository.deletar(id);
+    }
+
+    /**
+     * Metodo para atualizar o crédito na lista
+     * 
+     * @param credito que será aatulizado
+     * @return retorna o crédito após atualizar a lista
+     */
+    public Credito atualizar(Integer id, Credito credito) {
+
+        credito.setId(id);
+        return creditoRepository.atualizar(credito);
+
+    }
+}
